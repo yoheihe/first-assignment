a83b698d582b66d9378fdbb8bcb9cf82569135d7
yohei Yuda <yuyu.history.journey@gmail.com>
2024-04-04 18:04:19 +0900

    オブジェクト挑戦中

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,35 @@ import './App.css';
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [pets, setPets] = useState(["しば犬", "秋田犬", "ポメラニアン", "チワワ"]);
+  const [member, setMember] = useState([
+    {id: 1, name: 'しば犬'},
+    {id: 2, name: '秋田犬'},
+    {id: 3, name: 'ポメラニアン'},
+    {id: 4, name: 'チワワ'},
+  ]);
+
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+    <div className="container" key={member.id}>
+      {member.map((member) => {
+        return (
+          <div className="member" key={member.id}>
+            <p>member.name</p>
+            </div>
+        );
+      })}
+      </div>
+  )
+
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <p>You clicked {count} times</p>
+  //     <button onClick={() => setCount(count + 1)}>
+  //       Click me
+  //     </button>
+  //   </div>
+  // );
 }
 
 export default App;
